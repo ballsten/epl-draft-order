16d4e30e9363cb185cc6dd5082f07f47e14e3b5e
Andrew Theaker <andrew@theaker.name>
2017-11-27 14:41:23 +0930

    Add download script and match data

---
+++ data/download_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Download the premier league data from football-data.co.uk
+mkdir matches
+
+wget -q http://www.football-data.co.uk/mmz4281/9394/E0.csv -O matches/9394.csv
+wget -q http://www.football-data.co.uk/mmz4281/9495/E0.csv -O matches/9495.csv
+wget -q http://www.football-data.co.uk/mmz4281/9596/E0.csv -O matches/9596.csv
+wget -q http://www.football-data.co.uk/mmz4281/9697/E0.csv -O matches/9697.csv
+wget -q http://www.football-data.co.uk/mmz4281/9798/E0.csv -O matches/9798.csv
+wget -q http://www.football-data.co.uk/mmz4281/9899/E0.csv -O matches/9899.csv
+wget -q http://www.football-data.co.uk/mmz4281/9900/E0.csv -O matches/9900.csv
+
+wget -q http://www.football-data.co.uk/mmz4281/0001/E0.csv -O matches/0001.csv
+wget -q http://www.football-data.co.uk/mmz4281/0102/E0.csv -O matches/0102.csv
+wget -q http://www.football-data.co.uk/mmz4281/0203/E0.csv -O matches/0203.csv
+wget -q http://www.football-data.co.uk/mmz4281/0304/E0.csv -O matches/0304.csv
+wget -q http://www.football-data.co.uk/mmz4281/0405/E0.csv -O matches/0405.csv
+wget -q http://www.football-data.co.uk/mmz4281/0506/E0.csv -O matches/0506.csv
+wget -q http://www.football-data.co.uk/mmz4281/0607/E0.csv -O matches/0607.csv
+wget -q http://www.football-data.co.uk/mmz4281/0708/E0.csv -O matches/0708.csv
+wget -q http://www.football-data.co.uk/mmz4281/0809/E0.csv -O matches/0809.csv
+wget -q http://www.football-data.co.uk/mmz4281/0910/E0.csv -O matches/0910.csv
+
+wget -q http://www.football-data.co.uk/mmz4281/1011/E0.csv -O matches/1011.csv
+wget -q http://www.football-data.co.uk/mmz4281/1112/E0.csv -O matches/1112.csv
+wget -q http://www.football-data.co.uk/mmz4281/1213/E0.csv -O matches/1213.csv
+wget -q http://www.football-data.co.uk/mmz4281/1314/E0.csv -O matches/1314.csv
+wget -q http://www.football-data.co.uk/mmz4281/1415/E0.csv -O matches/1415.csv
+wget -q http://www.football-data.co.uk/mmz4281/1516/E0.csv -O matches/1516.csv
+wget -q http://www.football-data.co.uk/mmz4281/1617/E0.csv -O matches/1617.csv
