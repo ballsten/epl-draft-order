7056266093e3bf68ec52fc344b7c8c02cc2a7ee4
Andrew Theaker <andrew@theaker.name>
2017-12-17 15:29:22 +0930

    Fixed the data extract
    teams with same point and goal diff are now sorted alphabetically. Not
    correct, but unlikely to be a huge problem

--- data/create_tables.py
+++ data/create_tables.py
@@ -1,5 +1,5 @@
 import os
-import pandas
+import pandas as pd
 from collections import defaultdict
 
 COLS = ["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"]
@@ -9,7 +9,7 @@ seasons = []
 def create_table(file):
     season_date = file.split("/")[-1].split(".")[0]
     print("processing season", season_date)
-    season = pandas.read_csv(file, sep=",", usecols=COLS)
+    season = pd.read_csv(file, sep=",", usecols=COLS)
     table = defaultdict(lambda: { 'points':0, 'goaldiff': 0})
     for index, row in season.iterrows():
         table[row.HomeTeam]['points'] += 3 if row.FTR == "H" else 1 if row.FTR == "D" else 0
@@ -22,9 +22,10 @@ def create_table(file):
         v['team'] = k
         output.append(v)
 
-    df = pandas.DataFrame(output)
-    df['posScore'] = df['points']+(200+df['goaldiff'])/100
-    df['position'] = df['posScore'].rank(ascending=False)
+    df = pd.DataFrame(output)
+    df['posScore'] = (df['points']+(200+df['goaldiff'])/100).astype(str) + df['team']
+    df['position'] = pd.to_numeric(df['posScore'].rank(ascending=False), downcast="integer")
+
     df['season'] = season_date
     df = df[['position', 'season', 'team', 'points', 'goaldiff']]
     df = df.sort_values(['position'])
@@ -34,4 +35,4 @@ for root, dirs, files in os.walk("data/matches"):
     for file in files:
         create_table(os.path.join(root, file))
 
-pandas.concat(seasons).to_csv('data/tables/all.csv', quoting=2, index=False, float_format='%.f')
+pd.concat(seasons).to_csv('data/tables/all.csv', quoting=2, index=False, float_format='%.f')
