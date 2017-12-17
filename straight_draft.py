01b656785ea06865a5a19a8e4eaaace4cd72d0a3
Andrew Theaker <andrew@theaker.name>
2017-12-17 16:30:10 +0930

    Straight draft strategy

--- straight_draft.py
+++ straight_draft.py
@@ -9,22 +9,20 @@ import pandas as pd
 
 import CONFIG
 
-# create the pick array, index is the player number (0..x-1)
-picks = ((x+y*CONFIG.PLAYERS for y in range(CONFIG.PICKS)) for x in range(CONFIG.PLAYERS))
-
 # load the data file
 df = pd.read_csv('data/tables/all.csv', dtype = {'season': 'str'})
 
-# is just testing on one season for now
-SEASON = '0001'
-df = df.query('season == "{0}"'.format(SEASON))
+# create a list of the picks
+data = {
+    'position': list(range(1, CONFIG.PLAYERS*CONFIG.PICKS+1)),
+    'player' : list(x % 6 + 1 for x in range(CONFIG.PLAYERS*CONFIG.PICKS))
+}
 
-result = []
-for x in picks:
-    total = 0
-    for y in x:
-        total += int(df.query('position == {0}'.format(y+1))['points'])
+picks_df = pd.DataFrame(data)
+df = pd.merge(df, picks_df)
 
-    result.append(total)
 
-print(result)
+season_totals = df.groupby(['season', 'player'])['points'].sum()
+strategy_mean = season_totals.groupby('season').std().mean()
+
+print("The mean for the straight draft stragey is {0}".format(strategy_mean))
