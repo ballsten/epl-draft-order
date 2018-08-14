9be3e95f2a26fa64cb24e11f06a4a3a404dab781
Andrew Theaker <andrew@theaker.name>
2018-08-14 14:44:00 +1000

    Update team name mapping and remove player maps
    player name mapping now done in spreadsheet

--- scrape_538_epl.py
+++ scrape_538_epl.py
@@ -28,30 +28,10 @@ team_name_map = {
     'AFC Bournemouth': 'Bournemouth',
     'Newcastle': 'Newcastle',
     'West Bromwich Albion': 'West Brom',
-    'Swansea City': 'Swansea'
-}
-
-player_map = {
-    'Man City': 'Walsh',
-    'Man United': 'Jones',
-    'Chelsea': 'Reddy',
-    'Liverpool': 'Geary',
-    'Arsenal': 'Dorian',
-    'Burnley': 'Theaker',
-    'Tottenham': 'Theaker',
-    'Leicester': 'Dorian',
-    'Watford': 'Reddy',
-    'Everton': 'Geary',
-    'Huddersfield': 'Jones',
-    'Southampton': 'Geary',
-    'Brighton': 'Reddy',
-    'Crystal Palace': 'Dorian',
-    'West Ham': 'Walsh',
-    'Bournemouth': 'Walsh',
-    'Stoke': 'Jones',
-    'Newcastle': 'Theaker',
-    'West Brom': '',
-    'Swansea': ''
+    'Swansea City': 'Swansea',
+    'Fulham': 'Fulham',
+    'Wolverhampton': 'Wolves',
+    'Cardiff City': 'Cardiff'
 }
 
 def scrape_predictions():
@@ -65,7 +45,6 @@ def scrape_predictions():
     for row in rows:
         team = team_name_map[row.get_attribute('data-str')]
         prediction = {
-            'Player': player_map[team],
             'Team': team,
             'GD': row.find_elements_by_xpath("td[8]")[0].text,
             'Pts': row.find_elements_by_xpath("td[9]")[0].text
@@ -74,7 +53,7 @@ def scrape_predictions():
 
     df = pd.DataFrame(predictions)
 
-    df.to_csv("data/538.csv", sep='\t', columns=['Player', 'Team', 'Pts', 'GD'], index=False)
+    df.to_csv("data/538.csv", sep='\t', columns=['Team', 'Pts', 'GD'], index=False)
 
     driver.quit()
 
